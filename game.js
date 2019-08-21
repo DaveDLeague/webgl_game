1dac531f559f226cdd4dab2aa53d60bcd61b35d2
Dave <dave@Daves-MacBook-Pro.local>
2019-08-21 11:57:26 -0700

    adding meshes

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ var gl;
 var buttonDiv;
 var codeDiv;
 
+var gameCamera;
+
 window.onload = function(){
     buttonDiv = document.getElementById('buttonDivID');
     codeDiv = document.getElementById('codeDivID');
@@ -19,7 +21,31 @@ window.onload = function(){
     canvas.style.border = 'solid';
 
     windowResize();
-    initCanvas();
+
+    gl = canvas.getContext('webgl2');
+    gl.clearColor(0, 1, 1, 1);    
+
+    gameCamera = new Camera();
+    gameCamera.setPerspectiveProjection(70.0, canvas.width / canvas.height, 0.001, 1000.0);
+    gameCamera.position.z = 5;
+    gameCamera.updateView();
+
+    let verts = [
+        -0.5, -0.5, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0,
+        -0.5,  0.5, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0,
+         0.5,  0.5, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0,
+    ];
+
+    let inds = [
+        0, 1, 2,
+    ];
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    initTexturedMeshRenderer();
+    
+    let msh = createTexturedMesh(verts, inds);
+    prepareTexturedMeshRenderer();
+    renderTexturedMesh(msh, gameCamera);
 }
 
 function windowResize(){
@@ -31,11 +57,4 @@ function windowResize(){
     canvas.style.left = window.innerWidth * 0.25;
     canvas.width = window.innerWidth * 0.96 * 0.75;
     canvas.height = window.innerHeight * 0.96 * 0.75;
-}
-
-function initCanvas(){
-    gl = canvas.getContext('webgl2');
-    gl.clearColor(0, 1, 1, 1);
-    initTexturedMeshRenderer();
-    
 }
--- textured_mesh_renderer.js
+++ textured_mesh_renderer.js
@@ -1,54 +1,132 @@
+const tmVertShader = "#version 300 es\n\
+in vec3 position;\n\
+in vec3 normal;\n\
+in vec2 uvCoordinate;\n\
+uniform mat4 cameraViewMatrix;\n\
+out vec3 norm;\n\
+out vec2 uv;\n\
+void main(){\
+    norm = normal;\
+    uv = uvCoordinate;\
+    gl_Position = cameraViewMatrix * vec4(position, 1.0);\
+}\
+";
+
+const tmFragShader =  "#version 300 es\n\
+precision mediump float;\n\
+in vec3 norm;\
+out vec4 finalColor;\n\
+void main(){\
+    finalColor = vec4(1, 0, 0, 1);\
+}\
+";
+
+class TexturedMesh{
+    constructor(){
+        this.position = new Vector3();
+        this.orientation = new Quaternion();
+        this.totalIndices = 0;
+        this.indexOffset = 0;
+    }
+}
+
 var tmShader;
 var tmVao;
 var tmVbo;
 var tmIbo;
 
 var tmPositionID;
+var tmNormalID;
+var tmUvID;
 
-const tmVertShader = "#version 300 es\n\
-in vec3 position;\n\
-void main(){\
-gl_Position = vec4(position, 1.0);\
-}\
-";
-const tmFragShader =  "#version 300 es\n\
-precision mediump float;\n\
-out vec4 finalColor;\n\
-void main(){\
-finalColor = vec4(1, 0, 0, 1);\
-}\
-";
+var tmCameraViewMatrixID;
+var tmModelViewMatrixID;
+
+var tmIndexBufferSize;
+var tmVertexBufferSize;
 
 function initTexturedMeshRenderer(){
     tmShader = compileGLShader(gl, tmVertShader, tmFragShader);
     gl.useProgram(tmShader);
 
     tmPositionID = gl.getAttribLocation(tmShader, "position");
+    tmNormalID = gl.getAttribLocation(tmShader, "normal");
+    tmUvID = gl.getAttribLocation(tmShader, "uvCoordinate");
+
+    tmCameraViewMatrixID = gl.getUniformLocation(tmShader, "cameraViewMatrix");
 
     tmVao = gl.createVertexArray();
     gl.bindVertexArray(tmVao);
 
-    let verts = [
-        -0.5, -0.5, 0.0,
-        -0.5,  0.5, 0.0,
-         0.5,  0.5, 0.0, 
-         0.5, -0.5, 0.0,
-    ];
-
-    let inds = [
-        0, 1, 2, 2, 3, 0
-    ];
+    tmVertexBufferSize = 0;
+    tmIndexBufferSize = 0;
 
     tmVbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, tmVbo);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, 0, gl.STATIC_DRAW);
     gl.enableVertexAttribArray(tmPositionID);
-    gl.vertexAttribPointer(tmPositionID, 3, gl.FLOAT, gl.FALSE, 0, 0);
+    gl.enableVertexAttribArray(tmNormalID);
+    gl.enableVertexAttribArray(tmUvID);
+    gl.vertexAttribPointer(tmPositionID, 3, gl.FLOAT, gl.FALSE, 32, 0);
+    gl.vertexAttribPointer(tmNormalID, 3, gl.FLOAT, gl.FALSE, 32, 12);
+    gl.vertexAttribPointer(tmUvID, 2, gl.FLOAT, gl.FALSE, 32, 24);
     
     tmIbo = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tmIbo);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(inds), gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, 0, gl.STATIC_DRAW);
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
+}
+
+function prepareTexturedMeshRenderer(){
+    gl.useProgram(tmShader);
+    gl.bindVertexArray(tmVao);
+}
+
+function renderTexturedMesh(mesh, camera){
+    gl.uniformMatrix4fv(tmCameraViewMatrixID, gl.FALSE, camera.viewMatrix.m);
+    gl.drawElements(gl.TRIANGLES, mesh.totalIndices, gl.UNSIGNED_INT, mesh.indexOffset);
+}
+
+function createTexturedMesh(vertices, indices){
+    gl.bindVertexArray(tmVao);
+    verticesSize = vertices.length * 4;
+    indicesSize = indices.length * 4;
+    let nvbo = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, nvbo);
+    gl.bufferData(gl.ARRAY_BUFFER, tmVertexBufferSize + verticesSize, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.COPY_READ_BUFFER, tmVbo);
+    gl.copyBufferSubData(gl.COPY_READ_BUFFER, gl.ARRAY_BUFFER, 0, 0, tmVertexBufferSize);
+    gl.bufferSubData(gl.ARRAY_BUFFER, tmVertexBufferSize, new Float32Array(vertices));
+    gl.deleteBuffer(tmVbo);
+    tmVbo = nvbo;
+
+    let startIndex = tmVertexBufferSize / (4 * 8);
+    for(let i = 0; i < indices.length; i++){
+        indices[i] += startIndex;
+    }
+
+    let nibo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, nibo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indicesSize + tmIndexBufferSize, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.COPY_READ_BUFFER, tmIbo);
+    gl.copyBufferSubData(gl.COPY_READ_BUFFER, gl.ELEMENT_ARRAY_BUFFER, 0, 0, tmIndexBufferSize);
+    gl.bufferSubData(gl.ELEMENT_ARRAY_BUFFER, tmIndexBufferSize, new Uint32Array(indices));
+    gl.deleteBuffer(tmIbo);
+    tmIbo = nibo;
+
+    gl.enableVertexAttribArray(tmPositionID);
+    gl.enableVertexAttribArray(tmNormalID);
+    gl.enableVertexAttribArray(tmUvID);
+    gl.vertexAttribPointer(tmPositionID, 3, gl.FLOAT, gl.FALSE, 32, 0);
+    gl.vertexAttribPointer(tmNormalID, 3, gl.FLOAT, gl.FALSE, 32, 12);
+    gl.vertexAttribPointer(tmUvID, 2, gl.FLOAT, gl.FALSE, 32, 24);
+
+    let tm = new TexturedMesh();
+    tm.totalIndices = indices.length;
+    tm.indexOffset = tmIndexBufferSize;
+    tmVertexBufferSize += verticesSize;
+    tmIndexBufferSize += indicesSize;
+    
+    return tm;
 }
